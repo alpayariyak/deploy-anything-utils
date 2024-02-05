d17dca43e5e156da0a13ad634089ec838199b8b1
alpayariyak <aariyak@wpi.edu>
2024-02-05 16:11:47 -0500

    Added model.chat_template

--- model-metadata/example.py
+++ model-metadata/example.py
@@ -6,4 +6,5 @@ if __name__ == "__main__":
     model = HFModel(model_repo, hf_token)
     print(f"Memory sizes in GB: {model.memory_sizes}")
     print(f"Model has chat template: {model.model_has_chat_template}")
+    print(f"Chat template: {model.chat_template}")
     print(f"Model max length: {model.max_length}")
--- model-metadata/hfmodel.py
+++ model-metadata/hfmodel.py
@@ -6,15 +6,17 @@ class HFModel:
         self.model_repo = model_repo
         self.model = get_model(model_repo, library="transformers", access_token=hf_token or None)
         self.memory_sizes = self._get_memory_sizes()
-        self.model_has_chat_template = self._has_chat_template(access_token=hf_token or None)
+        self.chat_template = self._get_chat_template(access_token=hf_token or None)
+        self.model_has_chat_template = bool(self.chat_template)
         self.max_length = self._get_max_length()
         
-    def _has_chat_template(self, access_token=None):
+    def _get_chat_template(self, access_token=None):
         tokenizer = AutoTokenizer.from_pretrained(self.model_repo, access_token=access_token)
-        return bool(tokenizer.chat_template)
+        return tokenizer.chat_template
         
     def _get_max_length(self):
         return self.model.config.max_position_embeddings
+    
     def _get_memory_sizes(self):
         memory_sizes = calculate_memory(self.model, ["float32", "float16/bfloat16", "int8", "int4"])
         memory_dict = {dtype_info['dtype']: dtype_info["Inference (GB)"] for dtype_info in memory_sizes}
